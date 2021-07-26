444cec5e0cd5d4420425b369a83845b1ca96ec04
Isadora Carolina Siebert <isaahsc@gmail.com>
2021-07-26 18:11:27 -0300

    Mudança no sketch.

--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
+var train = true;
+
 function setup() {
     createCanvas(500, 500);
     background(0);
-    rede = new RedeNeural(2, 3, 1);
-    data = {
-        inputs: [
-            [1, 1],
+
+    nn = new RedeNeural(2, 3, 1);
+
+    // XOR Problem
+    dataset = {
+        inputs:
+            [[1, 1],
             [1, 0],
             [0, 1],
-            [0, 0]
-        ],
-        outputs: [
-            [0],
+            [0, 0]],
+        outputs:
+            [[0],
             [1],
             [1],
-            [0]
-        ]
+            [0]]
     }
 }
 
-var treino = true;
-
-function desenhar() {
-    if (treino) {
+function draw() {
+    if (train) {
         for (var i = 0; i < 10000; i++) {
-            var index = floor(randomize(4));
-            rede.treino(data.inputs[index], data.outputs[index]);
+            var index = floor(random(4));
+            nn.treino(dataset.inputs[index], dataset.outputs[index]);
         }
-        if (rede.predict([0, 0])[0] < 0.04 && rede.predict([1, 0])[0] > 0.98) {
-            treino = false;
+        if (nn.predict([0, 0])[0] < 0.04 && nn.predict([1, 0])[0] > 0.98) {
+            train = false;
             console.log("FIM");
         }
     }
